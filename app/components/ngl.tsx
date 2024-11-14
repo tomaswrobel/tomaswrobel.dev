424572dc2505ebb530bc61f2e736c3f24c192d82
Tomáš Wróbel <mail@tomaswrobel.dev>
2024-11-14 23:35:00 +0100

    NGL question

--- app/components/ngl.tsx
+++ app/components/ngl.tsx
@@ -2,12 +2,18 @@
 import type {FormEventHandler, FunctionComponent} from "react";
 
 const submit: FormEventHandler<HTMLFormElement> = async e => {
-	const form = e.currentTarget;
 	e.preventDefault();
+
+	const form = e.currentTarget;
+	const slug = form.question.value;
+	const question = form.answer.value;
 
 	const response = await fetch("/ngl", {
 		method: "POST",
-		body: form.message.value,
+		body: JSON.stringify({
+			gameSlug: slug === "anonymous" ? "" : slug,
+			question,
+		}),
 	});
 
 	if (response.ok) {
@@ -24,7 +30,18 @@ const Form: FunctionComponent = () => (
 		<div className="row">
 			<div className="col-md-12">
 				<div className="form-group">
-					<textarea name="message" placeholder="Message *" rows={6} className="form-control" required />
+					<select name="question" className="form-control" required>
+						<option value="anonymous">Send me anonymous messages!</option>
+						<option value="confessions">Send me anonymous confessions</option>
+						<option value="3words">Describe me in 3 words, anonymously</option>
+						<option value="neverhave">Send me a never have I ever, anonymously</option>
+						<option value="tbh">If you could change anything about me, what would it be?</option>
+					</select>
+				</div>
+			</div>
+			<div className="col-md-12">
+				<div className="form-group">
+					<textarea name="answer" placeholder="Message *" rows={6} className="form-control" required />
 				</div>
 			</div>
 			<div className="col-md-12">
--- app/ngl/route.ts
+++ app/ngl/route.ts
@@ -1,32 +1,13 @@
-import {cookies} from "next/headers";
 import type {NextRequest} from "next/server";
 
-function getDeviceId() {
-    const store = cookies();
-    const stored = store.get("deviceId");
-
-    if (stored) {
-        return stored.value;
-    }
-
-    const uuid = crypto.randomUUID();
-
-    store.set({
-        name: "deviceId",
-        sameSite: "strict",
-        value: uuid,
-    });
-
-    return uuid;
-}
-
 export async function POST(request: NextRequest) {
+	const data = await request.json();
+
     return fetch("https://ngl.link/api/submit", {
         method: "POST",
         body: new URLSearchParams({
-            gameSlug: "",
-            deviceId: getDeviceId(),
-            question: await request.text(),
+			...data,
+            deviceId: crypto.randomUUID(),
             username: process.env.NGL_USERNAME!,
             referrer: `https://${process.env.VERCEL_URL}`,
         })
